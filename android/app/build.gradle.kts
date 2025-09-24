44b1cca39f80d559f2992500dd9a01b1e880b93a
LucylleMakachia <lucylemakachia@gmail.com>
2025-09-24 13:29:24 +0300

    second commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,10 @@
+import org.gradle.api.tasks.Delete
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -11,7 +14,6 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        // Enable core library desugaring required for some plugins like flutter_local_notifications
         isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -22,7 +24,6 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID
         applicationId = "com.example.afrisight"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
@@ -31,9 +32,7 @@ android {
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with debug keys for now, so `flutter run --release` works.
+        getByName("release") {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -44,6 +43,5 @@ flutter {
 }
 
 dependencies {
-    // Add core library desugaring dependency for Java 8+ API support
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
